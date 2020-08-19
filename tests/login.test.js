239387c58ae4e8659e35d3c3d798a774a8a6cfbe
GirlesaQuinteroR <46802833+GirlesaQuinteroR@users.noreply.github.com>
2020-08-19 16:31:45 -0500

    Login test

--- tests/login.test.js
+++ tests/login.test.js
@@ -7,6 +7,23 @@ fixture `Login Test`
 test ('User cannot login with invalid credentials', async t =>{
     const signInButton = Selector('#user_login')
     await t.click(signInButton)
+
+    const loginForm = Selector('#login_form')
+    await t.expect(loginForm.exists).ok()
+
+    const userNameInput = Selector('#user_login')
+    const passwordInput = Selector('#user_password')
+    await t.typeText(userNameInput, 'Invalid UserName',{ paste: true })
+    await t.typeText(passwordInput, 'Invalid Password',{ paste: true })
+
+    const submitBtn = Selector('.btn-primary')
+    await t.click(submitBtn)
+
+    const errorMessage = Selector('.alert-error').innerText
+    await t.expect(errorMessage).contains('Login and/or password are wrong.')
+
+
+
 }
 
 )
